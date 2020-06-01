e332ef4f0547281796362793dbcedb893de3dccf
Sean Downs <spdowns@protonmail.com>
2020-06-01 13:28:56 -0700

    init project. adds writing functions practice to js. runs in browser test complete.
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: Tre Sonna <tresonna@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const saySomething = function(whatToSay) {
+  alert(whatToSay);
+  };
+  const multiply = function(number1, number2) {
+  return number1 * number2;
+  };
+  saySomething("Hey");
+  multiply (9, 4);
+  const result = multiply(9, 4);
+  alert(result);
+
+  
