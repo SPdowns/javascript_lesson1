34f1751a1ef9919bf9c34590f2272f6d2ae93ba6
Sean Downs <spdowns@protonmail.com>
2020-06-01 13:53:03 -0700

    add a function that takes thee inputs from a prompt
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: Tre Sonna <tresonna@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
-const saySomething = function(whatToSay) {
-  alert(whatToSay);
-  };
-  const multiply = function(number1, number2) {
-  return number1 * number2;
-  };
-  saySomething("Hey");
-  multiply (9, 4);
-  const result = multiply(9, 4);
-  alert(result);
+// const saySomething = function(whatToSay) {
+//   alert(whatToSay);
+// };
 
+// const multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
   
+// saySomething("Hey");
+
+// multiply (9, 4);
+
+// const result = multiply(9, 4);
+//   alert(result);
+
+const age = function(whatAge) {
+  prompt(whatAge);
+};
+
+const name = function(whatName) {
+  prompt(whatName);
+};
+
+const food = function(favFood) {
+  prompt(favFood);
+};
+
+age("what your age");
+
+name("what is your name");
+
+food("what is your favorite food");
+
+alert("Hello " + name + "you are " + age + "years old and your favorite food is " + food);
